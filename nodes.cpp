d2da5cc15e8ebef43de75cac489c2389477da114
Devin Mohr <128556500+devxinvestor@users.noreply.github.com>
2024-06-22 13:14:52 -0400

    nodes constructor

--- nodes.cpp
+++ nodes.cpp
@@ -1,7 +1,6 @@
 #include "nodes.h"
 
 // Constructor for the Node
-// https://www.onlinegdb.com/vWp_hhaQlC
 Node::Node(std::string x, std::string y){
     // Initialize the tree height to 1
     height = 1;
